5271bdbafa09d99078f206b06f6f32317b5c0d22
RodLopezDev <rodrigolopezdev@gmail.com>
2025-04-12 21:59:48 -0500

    chore(project @RodLopezDev) 🔧 Config project with dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "127.0.0.1", // instead of 0.0.0.0
+    hmr: {
+      host: "127.0.0.1",
+      port: 24678, // default Vite HMR port
+    },
+    watch: {
+      usePolling: true, // this is key for WSL!
+    },
+  },
+});
